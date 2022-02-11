c4153362c0bcc825698b11d320b2dd768c4710fb
Salah-boutayeb <boutayebsalaheddin@gmail.com>
2022-02-11 02:43:49 +0100

    restapi for goals

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const colors = require("colors");
+const connectDB = require("./config/db");
 const { use } = require("./routes/goalRoutes");
 const { errorHandler } = require("./middleware/middleware");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5555;
+connectDB();
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
